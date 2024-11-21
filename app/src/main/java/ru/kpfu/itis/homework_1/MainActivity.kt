60faa61113cf986dd68513593161fe50c2135d73
KamilSab <kamilsab46@gmail.com>
2024-11-21 22:44:59 +0300

    hw2

--- app/src/main/java/ru/kpfu/itis/homework_1/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/homework_1/MainActivity.kt
@@ -1,21 +1,10 @@
 package ru.kpfu.itis.homework_1
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ru.kpfu.itis.R
 import ru.kpfu.itis.databinding.ActivityMainBinding
-import ru.kpfu.itis.homework_1.ui.theme.StudyTheme
+
 
 class MainActivity : AppCompatActivity() {
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Study"
 include(":app")
- 
+include(":homework2")
